5a20fef3ab43b1d22dd2493f3483d3ee14394484
Shane Williamson <williamson.shane.a@gmail.com>
2017-10-18 01:37:01 -0600

    Actually got a decent start, about page close

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCardModule, MatGridListModule, MatSidenavModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { PortfolioEntryComponent } from './portfolio-entry/portfolio-entry.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PortfolioComponent,
-    PortfolioEntryComponent
+    PortfolioEntryComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
