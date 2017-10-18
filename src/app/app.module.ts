e8283088d41b798b28d859ce99416c6ba075234d
Shane Williamson <williamson.shane.a@gmail.com>
2017-10-18 02:59:38 -0600

    Got the router mostly working..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { RouterModule, RouterOutlet, Routes } from '@angular/router';
 import { MatButtonModule, MatCardModule, MatGridListModule, MatSidenavModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-import { PortfolioEntryComponent } from './portfolio-entry/portfolio-entry.component';
 import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: 'about', component: HomeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: '**', component: AppComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     PortfolioComponent,
-    PortfolioEntryComponent,
     HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
@@ -23,7 +34,9 @@ import { HomeComponent } from './home/home.component';
     MatGridListModule,
     MatSidenavModule,
   ],
-  providers: [],
+  providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
